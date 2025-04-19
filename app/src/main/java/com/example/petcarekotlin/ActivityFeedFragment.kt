caf3d61529468832419ded3f37dbaa6b5d5fa48f
roshh4 <roshinidharmaraj@gmail.com>
2025-04-19 18:28:31 +0530

    login page and app layout

---
+++ app/src/main/java/com/example/petcarekotlin/ActivityFeedFragment.kt
@@ -0,0 +1,15 @@
+package com.example.petcarekotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ActivityFeedFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_activity_feed, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/petcarekotlin/HomepageLogsFragment.kt
@@ -0,0 +1,15 @@
+package com.example.petcarekotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomepageLogsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_homepage_logs, container, false)
+    }
+}
