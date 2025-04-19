35deae7217bfaf8aeee7560b12c5f9cf5a1526df
roshh4 <roshinidharmaraj@gmail.com>
2025-04-20 02:55:21 +0530

    added navigation

--- app/src/main/java/com/example/petcarekotlin/FooterFragment.kt
+++ app/src/main/java/com/example/petcarekotlin/FooterFragment.kt
@@ -1,59 +1,46 @@
 package com.example.petcarekotlin
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FooterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FooterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    interface OnFooterNavigationListener {
+        fun onFooterItemSelected(itemId: Int)
+    }
+
+    private var navListener: OnFooterNavigationListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        navListener = parentFragment as? OnFooterNavigationListener
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        navListener = null
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_footer, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FooterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FooterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bottomNav = view.findViewById<BottomNavigationView>(R.id.bottom_nav_menu)
+
+        bottomNav.setOnItemSelectedListener { item ->
+            navListener?.onFooterItemSelected(item.itemId)
+            true
+        }
     }
 }
--- app/src/main/java/com/example/petcarekotlin/HomePageFragment.kt
+++ app/src/main/java/com/example/petcarekotlin/HomePageFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class HomepageFragment : Fragment() {
+class HomePageFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
